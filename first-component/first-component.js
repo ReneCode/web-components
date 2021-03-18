80bbdd20630f0d734bf85eaaf8fbc9210d45a8e6
rene_mbp <harrymillermail@googlemail.com>
2021-03-18 23:25:06 +0100

    first-component with preact

---
+++ first-component/first-component.js
@@ -0,0 +1,54 @@
+// https://codesandbox.io/s/preact-web-components-forked-fjfqb?file=/src/index.js:0-28
+import { h } from "preact";
+import { useState, useEffect } from "preact/hooks";
+
+import register from "preact-custom-element";
+
+const NameList = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const loadNames = async () => {
+      try {
+        const url = "https://pokeapi.co/api/v2/pokemon";
+        const response = await fetch(url);
+        const json = await response.json();
+        const names = json.results.map((e) => ({ name: e.name, url: e.url }));
+        setPokemons(names);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    loadNames();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {pokemons.map((pokemon) => {
+          return <li>{pokemon.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const Button = ({ children, onClick }) => {
+  return <button onClick={onClick}>{children}</button>;
+};
+
+const FirstComponent = ({ name }) => {
+  const yeah = () => {
+    console.log("yeah!");
+  };
+
+  return (
+    <div>
+      <Button onClick={yeah}>CLICK MICH</Button>
+      <h1>Hello {name ? name : "world"}</h1>
+      <NameList></NameList>
+    </div>
+  );
+};
+
+register(FirstComponent, "x-first", ["name"]);
